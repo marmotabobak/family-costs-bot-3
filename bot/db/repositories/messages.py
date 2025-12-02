8beef8a008260194ecf54173f826b90da9ee1b03
Igor Urvantsev <igurvantsev@ozon.ru>
2025-12-02 14:32:52 +0300

    first business-logic: save_message() to save message to DB

---
+++ bot/db/repositories/messages.py
@@ -0,0 +1,20 @@
+from bot.db.models import Message
+from sqlalchemy.ext.asyncio import AsyncSession
+
+async def save_message(
+        session: AsyncSession,
+        user_id: int,
+        text: str,
+) -> Message:
+    """Сохраняет сообщение в БД."""
+
+    message = Message(
+        user_id=user_id,
+        text=text,
+    )
+
+    session.add(message)
+    await session.commit()
+    await session.refresh(message)  # Загружаем id и created_at из БД после коммита.
+
+    return message
