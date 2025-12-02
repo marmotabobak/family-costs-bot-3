b7b4cf1ee1695566f4893e311d557a3508343949
Igor Urvantsev <igurvantsev@ozon.ru>
2025-12-02 15:00:37 +0300

    context manager for getting SQLAlchemy async session

---
+++ bot/db/dependencies.py
@@ -0,0 +1,15 @@
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import AsyncSession
+from collections.abc import AsyncGenerator
+
+from bot.db.session import async_session_maker
+
+@asynccontextmanager
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """Асинхронный менеджер контекста, возвращающий сессию."""
+
+    session = async_session_maker()
+    try:
+        yield session
+    finally:
+        await session.close()
