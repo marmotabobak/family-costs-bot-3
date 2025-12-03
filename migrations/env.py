24e3dfff83abe74f075ef4e05990bc8c29f6ed2a
Igor Urvantsev <igurvantsev@ozon.ru>
2025-12-03 22:09:05 +0300

    Add and adjust alembic; create alembic migration: messages table is created

---
+++ migrations/env.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import pool
+from alembic import context
+
+from bot.config import settings
+from bot.db.base import Base
+
+# VERY IMPORTANT: import models so they register in Base.metadata
+from bot.db import models  # noqa: F401
+
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def get_url() -> str:
+    return settings.database_url
+
+
+def run_migrations_offline() -> None:
+    url = get_url()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+        compare_server_default=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    engine = create_async_engine(get_url(), poolclass=pool.NullPool)
+
+    async with engine.connect() as conn:
+        await conn.run_sync(do_run_migrations)
+
+    await engine.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
